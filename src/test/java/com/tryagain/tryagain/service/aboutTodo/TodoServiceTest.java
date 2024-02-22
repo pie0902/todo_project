b74d4a4154b09fab3d0caace7aa0b6932ac29eab
pie0902 <zhfoqkq@gmail.com>
2024-02-22 18:05:35 +0900

    테스트 코드 작성 중

--- src/test/java/com/tryagain/tryagain/service/aboutTodo/TodoServiceTest.java
+++ src/test/java/com/tryagain/tryagain/service/aboutTodo/TodoServiceTest.java
@@ -9,30 +9,45 @@ import com.tryagain.tryagain.dto.aboutTodo.UpdateArticleRequest;
 import com.tryagain.tryagain.dto.aboutUser.UserSignupRequestDto;
 import com.tryagain.tryagain.repository.TodoRepository;
 import com.tryagain.tryagain.repository.UserRepository;
+import com.tryagain.tryagain.security.CustomUserDetails;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 @ExtendWith(MockitoExtension.class)
+
 class TodoServiceTest {
 
     @Mock
     TodoRepository todoRepository;
     @Mock
     UserRepository userRepository;
+    private final String userEmail = "testcode@naver.com";
     @InjectMocks
     TodoService todoService;
+
     @Test
     @DisplayName("게시글 전체 조회")
     void getAll() {
@@ -68,35 +83,5 @@ class TodoServiceTest {
         assertEquals(testArticle.getTitle(), foundArticle.getTitle());
         assertEquals(testArticle.getContent(), foundArticle.getContent());
     }
-    @Test
-    @DisplayName("게시글 수정")
-    void update(){
-        //given
-        Long testArticleId = 1L;
-        String email = "testcode@naver.com";
-        UpdateArticleRequest updateArticleRequest = new UpdateArticleRequest("test","test");
-        UserSignupRequestDto userSignupRequestDto = new UserSignupRequestDto("test","123","testcode@naver.com");
-        User articleUser = new User(userSignupRequestDto);
-        //수정할 게시글을 만든다.
-        AddArticleRequest addArticleRequest = new AddArticleRequest("beforTest","beforTest");
-        Article beforArticl = addArticleRequest.toEntity();
-        beforArticl.setId(testArticleId);
-        beforArticl.setUser(articleUser);
-        //업데이트할 dto로 객체 생성
-        Article afterArticle = updateArticleRequest.toEntity();
-        //업데이트할 게시글의 유저 생성 및 등록
-        afterArticle.setUser(articleUser);
-        given(todoRepository.findById(testArticleId)).willReturn(Optional.of(beforArticl));
-        given(todoRepository.save(any(Article.class))).willReturn(afterArticle);
-
-        //when
-        Article result = todoService.update(testArticleId, updateArticleRequest,email);
-
-        //then
-        assertNotNull(result);
-        assertEquals(updateArticleRequest.getTitle(), result.getTitle());
-        assertEquals(updateArticleRequest.getContent(), result.getContent());
-    }
-
-
 }
+
