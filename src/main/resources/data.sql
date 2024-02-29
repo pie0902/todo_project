1f241df4bc9ce3e37651618ba104b4e0c3ee340d
pie0902 <zhfoqkq@gmail.com>
2024-02-29 18:30:16 +0900

    merge 수정

--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO ARTICLE (title, content,created_at,updated_at) VALUES ('제목1', '내용1a',NOW(),NOW());
-INSERT INTO ARTICLE (title, content,created_at,updated_at) VALUES ('제목1', '내용1a',NOW(),NOW());
-INSERT INTO ARTICLE (title, content,created_at,updated_at) VALUES ('제목1', '내용1a',NOW(),NOW());
